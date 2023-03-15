07dc927f1e39b55f87e100b82eb5949fd7891468
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-15 16:19:55 +0600

    database connection is made

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.DB_URL as string).then(() => {
+  console.log("database is connected");
+});
 
 export default app;
