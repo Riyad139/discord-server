d7f95b1e58be14eb7a36cf47399b72cd31d49e3b
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-16 02:46:35 +0600

    now register function is updated

--- src/Controller/userController.ts
+++ src/Controller/userController.ts
@@ -1,1 +1,41 @@
-export const getUser: Controller = async (req, res, next) => {};
+import User from "../Models/User";
+import * as argon from "argon2";
+import * as json from "jsonwebtoken";
+export const registerUser: Controller = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const check = await User.findOne({ email });
+
+    const hashedPassword = await argon.hash(password);
+    console.log(check);
+    if (check !== null) {
+      return res.status(409).send("email is already in use");
+    }
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    /////tokken
+
+    const token = json.sign(
+      { id: user._id },
+      process.env.TOKEN_SECRETKEY as string,
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    res.status(201).json({
+      token,
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    res.status(501).send("internal error");
+  }
+};
+
+
