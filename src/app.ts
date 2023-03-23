bbafc6a30dddfd948ff1771fa9653a89f105bc64
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-24 05:45:18 +0600

    package name is updated and friend route is added

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./Routes/authRoute";
+import friendRoute from "./Routes/friendRoute";
 import cors from "cors";
 import addUserToReg from "./MiddleWare/addUserToReg";
 dotenv.config();
@@ -9,8 +10,9 @@ dotenv.config();
 const app = express();
 app.use(cors({ origin: process.env.HOST, credentials: true }));
 app.use(express.json());
-//app.use(addUserToReg);
+app.use(addUserToReg);
 app.use(authRoute);
+app.use(friendRoute);
 
 mongoose.connect(process.env.DB_URL as string).then(() => {
   console.log("database is connected");
