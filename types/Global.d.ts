f6c9097ffbc367ed1035f4c1b0472dd9d8902e08
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-24 05:47:57 +0600

    type added to req

--- types/Global.d.ts
+++ types/Global.d.ts
@@ -1,5 +1,11 @@
 import { RequestHandler } from "express";
+import { IUser } from "../src/Models/User";
 
 declare global {
   interface Controller extends RequestHandler {}
+  namespace Express {
+    export interface Request {
+      user: IUser;
+    }
+  }
 }
