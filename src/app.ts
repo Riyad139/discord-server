28de14209e4dbe074e3a4ad6e17d1f74c636c7af
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-23 09:03:37 +0600

    socket io is implemented

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./Routes/authRoute";
 import cors from "cors";
+import addUserToReg from "./MiddleWare/addUserToReg";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 app.use(cors({ origin: process.env.HOST, credentials: true }));
+app.use(express.json());
+//app.use(addUserToReg);
 app.use(authRoute);
 
 mongoose.connect(process.env.DB_URL as string).then(() => {
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,23 @@
 import app from "./app";
+import http from "http";
+import { Server, Socket } from "socket.io";
+import parseUser from "./store/parseUser";
+import { NextFunction } from "express";
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
 
-app.listen(process.env.PORT, () =>
+io.on("connection", (socket: Socket) => {
+  //console.log(socket.handshake.auth.token);
+  parseUser(socket);
+});
+
+server.listen(process.env.PORT, () =>
   console.log(`server is listening on port ${process.env.PORT}`)
 );
+
+export default server;
