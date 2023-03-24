a2dec6154623adce263a95b98a37cee3058205db
Riyad139 <riyad.ahmed1886@gmail.com>
2023-03-25 00:34:12 +0600

    now socket send friend request update to front end

---
+++ src/Routes/update/friendPendinghandler.ts
@@ -0,0 +1,26 @@
+import Invitation from "../../Models/Invitation";
+import User from "../../Models/User";
+import userStore, { getSocketInstance } from "../../store/store";
+
+const updatePendingUser = async (userId: string) => {
+  const targetUser = await User.findOne({ _id: userId });
+  if (!targetUser) return "error";
+  const pendingInvitation = await Invitation.find({
+    Receiver: targetUser._id,
+  }).populate("Sender", "_id username email");
+  if (pendingInvitation.length < 1) return "error";
+
+  const getUserConnectedIds = userStore.getSocketListByUserId(
+    targetUser._id.toString()
+  );
+  const io = getSocketInstance();
+
+  if (getUserConnectedIds) {
+    getUserConnectedIds.forEach((socketIds) => {
+      io.to(socketIds).emit("friend-request", {
+        pendingRequest: pendingInvitation,
+      });
+    });
+  }
+};
+export default updatePendingUser;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,7 @@
-import { Socket } from "socket.io";
+import { Server, Socket } from "socket.io";
 import * as jwt from "jsonwebtoken";
-class userStore {
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
+class CuserStore {
   private onlineUser = new Map();
   private _parseUserId(token: string) {
     const id = jwt.verify(token, process.env.TOKEN_SECRETKEY as string);
@@ -9,16 +10,36 @@ class userStore {
   }
   addUserToOnline(socket: Socket) {
     const id = this._parseUserId(socket.handshake.auth.token);
-    this.onlineUser.set(id, socket.id);
-    console.log(this.onlineUser);
+    this.onlineUser.set(socket.id, id);
   }
   removeUserFromOnline(socket: Socket) {
-    const id = this._parseUserId(socket.handshake.auth.token);
-    this.onlineUser.delete(id);
-    console.log(this.onlineUser);
+    this.onlineUser.delete(socket.id);
+  }
+  getSocketListByUserId(userId: string) {
+    let socketList: string[] = [];
+    this.onlineUser.forEach((value, key) => {
+      if (value === userId) socketList.push(key);
+    });
+    return socketList;
   }
 }
+let io: any;
 
-const user = new userStore();
+export const setIoInstance = (
+  socketInstance: Server<
+    DefaultEventsMap,
+    DefaultEventsMap,
+    DefaultEventsMap,
+    any
+  >
+) => {
+  io = socketInstance;
+};
 
-export default user;
+export const getSocketInstance = () => {
+  return io;
+};
+
+const userStore = new CuserStore();
+
+export default userStore;
