22dd86dd43ee9d524d261b22a87b4f105a80ef0f
Riyad139 <riyad.ahmed1886@gmail.com>
2023-04-15 04:05:10 +0600

    silly mistake

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.SOCKET_CORS as string,
-    allowedHeaders: ["access-control-Allow-origin"],
+    allowedHeaders: ["Access-Control-Allow-Origin"],
     methods: ["GET", "POST"],
   },
 });
